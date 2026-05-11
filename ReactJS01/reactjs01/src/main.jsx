6daff9629deb510a00460bc86761920199e6bce0
Trần Hồ Phương Nguyên <phng972005@gmail.com>
2026-05-11 21:14:11 +0700

    Frontend - ReactJS

---
+++ ReactJS01/reactjs01/src/main.jsx
@@ -0,0 +1,58 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import RegisterPage from './pages/register.jsx';
+import LoginPage from './pages/login.jsx';
+import UserPage from './pages/user.jsx';
+import HomePage from './pages/home.jsx';
+import './styles/global.css';
+import { AuthWrapper } from './components/context/auth.context.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: "user",
+        element: <UserPage />,
+      },
+    ]
+  },
+  {
+    path: "register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthWrapper>
+      <RouterProvider router={router} />
+    </AuthWrapper>
+  </React.StrictMode>,
+)
